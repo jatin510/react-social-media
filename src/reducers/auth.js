3cfa3f391c76e20e722c4f27c25f24386b5e75b2
Jagdish <jatin6972@gmail.com>
2020-08-10 00:45:18 +0530

    User login : 1

---
+++ src/reducers/auth.js
@@ -0,0 +1,28 @@
+import { LOGIN_START, LOGIN_FAIL } from '../actions/actionTypes';
+
+const initialState = {
+  user: {},
+  error: null,
+  isLoggedin: false,
+  inProgress: false,
+};
+export default function auth(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return { ...state, inProgress: true };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        isLoggedin: true,
+        error: false,
+        inProgress: false,
+      };
+
+    case LOGIN_FAIL:
+      return { ...state, inProgress: false, error: action.error };
+    default:
+      return state;
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import posts from './posts';
+import auth from './auth';
 
+// {posts : [], auth}
 export default combineReducers({
   posts,
+  auth,
 });

c0271f3cb5f40ea97c3098f8565dd582e404e0fb
Jagdish <jatin6972@gmail.com>
2020-08-09 20:23:28 +0530

    handle form data by react uncontrolled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.emailInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('this.emailInputRef ', this.emailInputRef);
+    console.log('this.passwordInputRef ', this.passwordInputRef);
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Login</span>
 
         <div className="field">
-          <input type="email" placeholder="enter your email" required />
+          <input
+            type="email"
+            placeholder="enter your email"
+            ref={this.emailInputRef}
+            required
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="enter your password" required />
+          <input
+            type="password"
+            placeholder="enter your password"
+            ref={this.passwordInputRef}
+            required
+          />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button onClick={this.handleFormSubmit}>Log In</button>
         </div>
       </form>
     );
